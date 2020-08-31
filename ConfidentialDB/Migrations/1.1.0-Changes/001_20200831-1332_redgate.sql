c2d0031d95b68896bf135136e9c4b36e0c5a6940
WIN2016\redgate <redgate@redgate.com>
2020-08-31 13:32:51 +0100

    dropped newproc

---
+++ ConfidentialDB/Migrations/1.1.0-Changes/001_20200831-1332_redgate.sql
@@ -0,0 +1,8 @@
+﻿-- <Migration ID="5f9ac62a-d5fb-42d9-8d3e-0007625bddcb" />
+GO
+
+PRINT N'Dropping [dbo].[newproc]'
+GO
+IF OBJECT_ID(N'[dbo].[newproc]', 'P') IS NOT NULL
+DROP PROCEDURE [dbo].[newproc]
+GO
